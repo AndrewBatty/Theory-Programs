61142ba0e17621a55f33be6d0eafaa7c0ef9ee82
Andrew Batty <21553@longroad.ac.uk>
2014-10-03 10:27:08 +0100

    ASCII Python exercises.

---
+++ ASCII Activity.py
@@ -0,0 +1,16 @@
+# Andrew Batty
+# ASCII Activity
+# Task 1
+# 03/10/2014
+
+print("This program will use an ASCII code entered by up and convert it into the equivalent text, or vise versa.")
+
+choice = input("Please enter either an 'ASCII' code, or 'text': ")
+
+if choice == "ASCII":
+    inputASCII = chr(MyInt) 
+    print("The ASCII code you have entered is: {0}".format(inputASCII))
+elif choice == "text":
+    inputText = ord(Mychar)
+    print("The text you have entered is: {0}".format(inputText))
+    
